8a4a9cda67b2e39452788ea463e666f5b2cbc378
markusg <mguehrs@gmail.com>
2013-05-06 14:02:46 +0200

    Finish sign in

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,30 @@
+module SessionsHelper
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  #Though it must be used with caution, the flexibility to add methods
+  # to built-in classes allows for extraordinarily natural additions to plain Ruby.
+  # Indeed, much of the elegance of Rails ultimately derives from the malleability
+  # of the underlying Ruby language.
+
+end
